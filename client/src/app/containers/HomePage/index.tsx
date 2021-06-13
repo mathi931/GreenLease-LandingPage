28b515171a2a4e82a4bbe5e49cbe7583d620331f
mathi931 <mesi.mathi@gmail.com>
2021-06-13 23:23:06 +0200

    about us section

--- client/src/app/containers/HomePage/index.tsx
+++ client/src/app/containers/HomePage/index.tsx
@@ -5,6 +5,7 @@ import { Navbar } from '../../components/navbar';
 import { TopSection } from './topSection';
 import { Marginer } from '../../components/marginer';
 import { LeaseSteps } from './leaseSteps';
+import { AboutUs } from './aboutUs';
 
 const PageContainer = styled.div`
 	${tw`
@@ -24,6 +25,9 @@ export function HomePage() {
 			<TopSection />
 			<Marginer direction='vertical' margin='7em' />
 			<LeaseSteps />
+			<Marginer direction='vertical' margin='7em' />
+			<AboutUs />
+			<Marginer direction='vertical' margin='7em' />
 		</PageContainer>
 	);
 }
